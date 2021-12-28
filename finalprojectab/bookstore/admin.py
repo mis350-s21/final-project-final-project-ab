bd7e972f6335e8d670312114d7cd850cf3970d6a
abdulazizalloughani <abdulaziz.alloughani@cba.ku.edu.kw>
2021-12-28 15:52:43 +0300

    models book 1st test

--- finalprojectab/bookstore/admin.py
+++ finalprojectab/bookstore/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display= ("price" , "name" , "slug")
+
+
+
+admin.site.register(Book , BookAdmin)
--- finalprojectab/bookstore/models.py
+++ finalprojectab/bookstore/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Book(models.Model): 
@@ -9,25 +10,25 @@ class Book(models.Model):
         (1,"In Cart"),
     )
 
-<<<<<<< Updated upstream
     category = models.CharField(max_length=50 , null=False)
     name = models.CharField(max_length=200, null=False , unique=True)
     slug = models.SlugField(max_length=200,unique=True  )
-    image = models.ImageField()
+    #image = models.ImageField()
     description = models.TextField(blank=True,)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     Cart = models.IntegerField(choices=CART, default=0)
 
+class Author(models.Model):
+    Author_name = models.CharField(max_length=50 ,unique=True )
+
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=50 , null=False)
+    customer_email = models.EmailField()
+    customer_num = models.IntegerField(null=False)
+    customer_address = models.CharField(max_length= 100 , null=False)
+
+
 # this is a test
 # part 2
 
-=======
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    body =models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    Cart = models.IntegerField(choices=CART, default=0)
-
->>>>>>> Stashed changes
