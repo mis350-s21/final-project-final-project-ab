716da669b4121fa19913d969828f0af2a2b03b9c
abdulazizalloughani <abdulaziz.alloughani@cba.ku.edu.kw>
2021-12-28 15:10:38 +0300

    a

--- finalprojectab/bookstore/models.py
+++ finalprojectab/bookstore/models.py
@@ -9,11 +9,13 @@ class Book(models.Model):
         (1,"In Cart"),
     )
 
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    body =models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=50 , null=False)
+    name = models.CharField(max_length=200, null=False , unique=True)
+    slug = models.SlugField(max_length=200,unique=True  )
+    image = models.ImageField()
+    description = models.TextField(blank=True,)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
     Cart = models.IntegerField(choices=CART, default=0)
 
 # this is a test
