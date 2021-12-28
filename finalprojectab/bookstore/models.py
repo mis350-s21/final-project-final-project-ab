c06d9e05f86a088f9521b520af1e7bad96bfa091
abdulazizalloughani <abdulaziz.alloughani@cba.ku.edu.kw>
2021-12-28 15:00:38 +0300

    test

--- finalprojectab/bookstore/models.py
+++ finalprojectab/bookstore/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+<<<<<<< Updated upstream
+=======
+
+class Book(models.Model): 
+
+    CART = (
+        (0,"Not in Cart"),
+        (1,"In Cart"),
+    )
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    body =models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    Cart = models.IntegerField(choices=CART, default=0)
+
+# this is a test
+>>>>>>> Stashed changes
