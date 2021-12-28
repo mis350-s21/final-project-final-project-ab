545be0e949ca046b93c4f79debb1cb8d7df0d606
BarrakKU <barrak.babtain@cba.ku.edu.kw>
2021-12-28 15:56:46 +0300

    b

--- finalprojectab/bookstore/models.py
+++ finalprojectab/bookstore/models.py
@@ -2,18 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Book(models.Model): 
-
-    CART = (
-        (0,"Not in Cart"),
-        (1,"In Cart"),
-    )
-
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    body =models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    Cart = models.IntegerField(choices=CART, default=0)
-
-    #test...
