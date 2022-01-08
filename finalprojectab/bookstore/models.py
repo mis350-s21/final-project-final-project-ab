0c043b21ca841c514c0b7399f07d190b79bf5bba
abdulazizalloughani <abdulaziz.alloughani@cba.ku.edu.kw>
2022-01-08 21:19:47 +0300

    a

--- finalprojectab/bookstore/models.py
+++ finalprojectab/bookstore/models.py
@@ -10,9 +10,6 @@ class Book(models.Model):
         (1,"In Cart"),
     )
 
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    body =models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     Cart = models.IntegerField(choices=CART, default=0)
