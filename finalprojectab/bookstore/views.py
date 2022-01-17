087e20426feaac65e46c4b7f80051380ed8217f0
abdulazizalloughani <abdulaziz.alloughani@cba.ku.edu.kw>
2022-01-17 19:49:48 +0300

    a

--- finalprojectab/bookstore/views.py
+++ finalprojectab/bookstore/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-
+from .models import Book, Review
+from .forms import OrderForm, RequestForm , Contact_usForm , ReviewForm
 # Create your views here.
 
 def greeting(request):
